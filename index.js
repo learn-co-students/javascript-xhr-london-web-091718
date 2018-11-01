e616574d82109e1c5a93d716cc3b1574513274ec
mal <1881633+akmalkhadir@users.noreply.github.com>
2018-11-01 11:06:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 function showRepositories(event, data) {
-    console.log(this.responseText)
-    let repoList = "<ul>"
-    for (var i = 0; i < this.responseText.length; i++) {
-        repoList += "<li>" + this.responseText[i]["name"] + "</li>"
-    }
-    repoList += "</ul>"
-
-    document.getElementById("repositories").innerHTML = repoList
+  var repos = JSON.parse(this.responseText);
+  console.log(repos);
+  const repoList = `<ul>${repos
+    .map(
+      r =>
+        "<li>" +
+        r.name +
+        ' - <a href="#" data-repo="' +
+        r.name +
+        '" onclick="getCommits(this)">Get Commits</a></li>'
+    )
+    .join("")}</ul>`;
+  document.getElementById("repositories").innerHTML = repoList;
 }
 
 function getRepositories() {
@@ -15,4 +20,27 @@ function getRepositories() {
 
   req.open("GET", "https://api.github.com/users/octocat/repos")
   req.send()
+}
+
+function getCommits(el) {
+  const name = el.dataset.repo;
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showCommits)
+  req.open("GET", "https://api.github.com/repos/octocat/" + name + "/commits")
+  req.send()
+}
+
+function showCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        "<li><strong>" +
+        commit.commit.author.name +
+        "</strong> - " +
+        commit.commit.message +
+        "</li>"
+    )
+    .join("")}</ul>`;
+  document.getElementById("commits").innerHTML = commitsList;
 }
